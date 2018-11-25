174b6035a6330e2c544a87c6e6f9ecc0ff3dce17
JCakboy <jcakboygames@gmail.com>
2018-11-24 19:08:48 -0800

    Test for future LVGL usage

--- Test - LVGL/src/initialize.cpp
+++ Test - LVGL/src/initialize.cpp
@@ -1,15 +1,5 @@
 #include "main.h"
 
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -17,10 +7,27 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+	/*
+	The V5 screen is 480 pixels wide by 272 pixels high.
+	We use 32 lines at the top for the menu bar, so the area available for user code is 480 x 240 pixels.
+	*/
 
-	pros::lcd::register_btn1_cb(on_center_button);
+	// Create and load the screen
+	lv_obj_t * screen = lv_obj_create(NULL, NULL);
+	lv_scr_load(screen);
+
+	lv_obj_t * statusText = lv_ta_create(screen, NULL);
+	lv_ta_set_cursor_type(statusText, LV_CURSOR_NONE);
+	lv_ta_set_one_line(statusText, true);
+	lv_ta_set_style(statusText, LV_TA_STYLE_BG, &lv_style_transp);
+	lv_ta_set_text(statusText, "Status: Initializing screen");
+
+	lv_obj_t * statusLine = lv_line_create(screen, NULL);
+	lv_point_t statusLinePoints[] = {{0, 32}, {480, 32}};
+	lv_line_set_points(statusLine, statusLinePoints, 2);
+
+
+
 }
 
 /**
