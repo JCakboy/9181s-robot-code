6dbda6f62ba43cd79b1ec467b87b3b919adeab4a
JCakboy <36982207+JCakboy@users.noreply.github.com>
2021-02-12 09:02:27 -0800

    Add POST

--- --Working/src/initialize.cpp
+++ --Working/src/initialize.cpp
@@ -6,6 +6,96 @@
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
+std::string post() {
+	bool pass = true;
+	int frontRightValue = ports::frontRightDrive->get_position();
+	int frontLeftValue = ports::frontLeftDrive->get_position();
+	int backRightValue = ports::backRightDrive->get_position();
+	int backLeftValue = ports::backLeftDrive->get_position();
+	int intakeMotorLeftValue = ports::intakeMotorLeft->get_position();
+	int intakeMotorRightValue = ports::intakeMotorRight->get_position();
+	int indexerValue = ports::indexer->get_position();
+	int flywheelValue = ports::flywheel->get_position();
+	ports::pid->driveStraight(2);
+	ports::intakeMotorLeft->move(2);
+	ports::intakeMotorRight->move(2);
+	ports::indexer->move(2);
+	ports::flywheel->move(2);
+	int timer = 0;
+	while (util::abs(frontRightValue - ports::frontRightDrive->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::frontRightDrive->move(-2);
+		else if (timer == 40)
+			return "frontRightDrive";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(frontLeftValue - ports::frontLeftDrive->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::frontLeftDrive->move(-2);
+		else if (timer == 40)
+			return "frontLeftDrive";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(backRightValue - ports::backRightDrive->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::backRightDrive->move(-2);
+		else if (timer == 40)
+			return "backRightDrive";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(backLeftValue - ports::backLeftDrive->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::backLeftDrive->move(-2);
+		else if (timer == 40)
+			return "backLeftDrive";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(intakeMotorRightValue - ports::intakeMotorRight->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::intakeMotorRight->move(-2);
+		else if (timer == 40)
+			return "intakeMotorRight";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(intakeMotorLeftValue - ports::intakeMotorLeft->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::intakeMotorLeft->move(-2);
+		else if (timer == 40)
+			return "intakeMotorLeft";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(indexerValue - ports::indexer->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::indexer->move(-2);
+		else if (timer == 40)
+			return "indexer";
+		pros::delay(5);
+	}
+	timer = 0;
+	while (util::abs(flywheelValue - ports::flywheel->get_position()) > 5) {
+		timer++;
+		if (timer == 20)
+			ports::flywheel->move(-2);
+		else if (timer == 40)
+			return "flywheel";
+		pros::delay(5);
+	}
+	return "Success";
+}
+
 void initialize() {
 	// Initialize the LLEMU
 	LCD::initializeLLEMU(ports::controllerMain, ports::controllerPartner);
@@ -53,6 +143,17 @@ void initialize() {
 	ports::gyroTask = new pros::Task(gyroTask, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Gyro");
 	// Start message debugging if the debugger is attached
 	ports::mhTask = new pros::Task(mhTask, NULL, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "Message Handler");
+
+	LCD::setStatus("Power on self-test...");
+	std::string postResult = post();
+	if (postResult != "Success")
+		LCD::setStatus("POST pass");
+	else {
+		LCD::setStatus("POST FAIL - " + postResult);
+		LCD::setControllerText("Check " + postResult);
+		ports::controllerMain->rumble("---");
+		pros::delay(3000);
+	}
 
 	LCD::setStatus("Initialization complete");
 }
